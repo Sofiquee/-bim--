5e095568e9a13b8769c9c97404d2eb8c52e7abea
Sofiquee <sonyakaryakina@gmail.com>
2024-11-13 23:43:34 +0300

    Delete лабораторная работа 1 directory

--- лабораторная работа 1/Task2.py
+++
@@ -1,7 +0,0 @@
-
-vol = 1.44 * 1024 * 1024
-pages = 100
-lines_per_page = 50
-chars_in_line = 25
-volume_for_char = 4
-print("Количество книг, помещающихся на дискету:", int(vol // (pages*lines_per_page*chars_in_line*volume_for_char) ))
